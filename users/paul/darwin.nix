54a66783a69a6ed28d863385e5b455df165481dd
Paul Chobert <paul@chobert.fr>
2023-01-26 15:20:24 +0100

    Some minor macos changes

--- users/paul/darwin.nix
+++ users/paul/darwin.nix
@@ -3,5 +3,13 @@
 {
   imports = [ ./common.nix ];
 
-  users.users = { paul= { home = "/Users/paul"; }; };
+  users.users = { paul = { home = "/Users/paul"; }; };
+
+  system = {
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToEscape = true;
+    };
+    defaults.trackpad.TrackpadThreeFingerDrag = true;
+  };
 }
