757c190a42b4aff1f70e8e6207172a8e91a3636c
Paul Chobert <paul@chobert.fr>
2026-03-03 22:58:03 +0100

    Set merge.ff to false for no-fast-forward merges by default

--- profiles/home-manager/git/default.nix
+++ profiles/home-manager/git/default.nix
@@ -26,6 +26,10 @@
           pretty = "format:%C(blue)%ad%Creset %C(yellow)%h%C(green)%d%Creset %C(blue)%s %C(magenta) [%an]%Creset";
         };
 
+        merge = {
+          ff = false;
+        };
+
         push = {
           autoSetupRemote = true;
         };
