033b166f5d8460a82487b2a50a2871554584b5ea
Paul Chobert <paul@chobert.fr>
2026-03-11 18:06:17 +0100

    Add vim, ruby-lsp, and relay extensions to VS Code with updated settings
    Switch theme to Tokyo Night Storm, enable relative line numbers,
    disable ligatures, and configure vim system clipboard integration.

--- profiles/home-manager/vscode/default.nix
+++ profiles/home-manager/vscode/default.nix
@@ -4,24 +4,35 @@
   programs.vscode = {
     enable = true;
 
+    mutableExtensionsDir = true;
+
     profiles.default.extensions = with pkgs.vscode-extensions; [
       jnoortheen.nix-ide
       mkhl.direnv
       enkia.tokyo-night
       editorconfig.editorconfig
+      vscodevim.vim
+      shopify.ruby-lsp
+    ] ++ [
+      (pkgs.vscode-utils.extensionFromVscodeMarketplace {
+        name = "relay";
+        publisher = "meta";
+        version = "2.5.1";
+        sha256 = "0z7sqzz78ga6majzpf5xhyw6c6881lb4d5cb7gilkyzf86ghnicn";
+      })
     ];
 
     profiles.default.userSettings = {
       "editor.fontFamily" = "'JetBrains Mono', 'monospace'";
       "editor.fontSize" = 14;
-      "editor.fontLigatures" = true;
+      "editor.fontLigatures" = false;
       "editor.minimap.enabled" = false;
       "editor.formatOnSave" = true;
       "editor.renderWhitespace" = "trailing";
       "editor.tabSize" = 2;
       "editor.wordWrap" = "off";
 
-      "workbench.colorTheme" = "Tokyo Night";
+      "workbench.colorTheme" = "Tokyo Night Storm";
       "workbench.startupEditor" = "none";
 
       "terminal.integrated.fontFamily" = "'JetBrains Mono'";
@@ -29,6 +40,9 @@
       "files.trimTrailingWhitespace" = true;
       "files.insertFinalNewline" = true;
       "files.trimFinalNewlines" = true;
+
+      "vim.useSystemClipboard" = true;
+      "editor.lineNumbers" = "relative";
     };
   };
 }
